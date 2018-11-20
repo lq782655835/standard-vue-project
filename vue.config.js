6d744bb34c7f848168f2fd2d1f1ea8e2583a7b95
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-11-20 10:53:18 +0800

    refactor vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,6 @@
-function resolve(dir) {
-    return Path.join(__dirname, dir)
-}
-
-const Path = require('path')
-
 module.exports = {
-    // 项目部署的基础路径
-    baseUrl: '/',
-
-    // 构建好的文件输出路径
-    outputDir: 'dist',
-
-    // 保存时使用 `eslint-loader` 检查
-    lintOnSave: true,
-
-    // 是否为生产环境构建生成 source map？
-    productionSourceMap: true,
+    // 是否为生产环境构建生成 source map
+    productionSourceMap: false,
 
     // CSS 相关选项
     css: {
@@ -26,22 +11,9 @@ module.exports = {
         }
     },
 
-    // webpack配置
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': resolve('src')
-            }
+    chainWebpack: config => {
+        if (process.env.npm_config_report) {
+            config.plugin('webpack-bundle-analyzer').use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
         }
     },
-
-    // 配置 webpack-dev-server 行为
-    devServer: {
-        open: process.platform === 'darwin',
-        host: '0.0.0.0',
-        port: 8080,
-        https: false,
-        // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli/cli-service.md#配置代理
-        proxy: null
-    }
 }
